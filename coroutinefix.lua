a446d2932bc32afc672a7bf3ea45a414a134081d
orenjiakira <orenjiakira@gmail.com>
2017-09-08 03:15:57 -0300

    Simplify coroutine fix

--- coroutinefix.lua
+++ coroutinefix.lua
@@ -1,11 +1,7 @@
 
-local _coroutine_resume = coroutine.resume
-coroutine.resume = function (...)
-  local state,result = _coroutine_resume(...)
-  if not state then
-    -- Output error message
-    error( tostring(result), 2 )
-  end
-  return state,result
+local _resume = coroutine.resume
+coroutine.resume = function(...)
+  return assert(_resume(...))
 end
+
 
